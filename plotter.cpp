0b35536397f576af3b5b3a77c64b34b929700441
Gautam <gautamrk@hotmail.com>
2017-10-05 00:47:34 +0530

    Hightlights point pointed by mouse.

--- plotter.cpp
+++ plotter.cpp
@@ -4,11 +4,13 @@
 #include<stdio.h>
 
 #define segments 100000
+int w = 1366, h = 768;
+int mouseX, mouseY;
 const float segmentlen = 1.0/segments;
 const float screenxstart = -5.0f,screenxstop = 5.0f;
 const float screenystart = -2.75f,screenystop = 2.75f;
 //Screen ranges from -5 to +5 on OpenGl coordinates
-float funcdata[20]={0},preplot[segments]={0};
+float funcdata[20]={0},y[segments]={0};
 int degree;
 float startx,stopx; //Range of x to be plotted
 float starty=INFINITY,stopy=-INFINITY;
@@ -56,34 +58,53 @@ void handleResize(int w, int h) {
 	glLoadIdentity();
 	gluPerspective(45.0, (double) w / (double) h, 1.0, 200.0);
 }
+
+void mouseMotion(int x, int y){
+	mouseX = x;
+}
+
 void precompute(){
 	int i;
 	float x = startx;
 	for(i=0;i<segments;i++){
-		preplot[i] = funcval(x);
-		//printf("%f ",funcval(x));
-		if(preplot[i]<starty){
-			starty = preplot[i];
+		y[i] = funcval(x);
+		if(y[i]<starty){
+			starty = y[i];
 		}
-		if(preplot[i]>stopy){
-		    stopy = preplot[i];	
+		if(y[i]>stopy){
+		    stopy = y[i];	
 		}
 		x += (stopx-startx)/segments;
 	}
-	//printf("ok");
-	/*for(i=0;i<1000;i++){
-		printf("%f ",preplot[i]);
-	}*/
+	
 }
-void drawScene() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -7.0f);
+void drawPointLoc(){
+	int i = ((float)mouseX*100000)/1366;
+	float x = 0.0f,fx = 0.0f;
+	x = screenxstart + 10.0f*(i)/100000;
+	fx = screenystart + 5.5*(y[i]-starty)/(stopy-starty);
+	//printf("%d %d %f\n",i,mouseX,x);
+	glPushMatrix();
+	glTranslatef(x,0.0f,0.0f);
+	glBegin(GL_LINES);
+		glColor3f(0.1f,0.6f,0.1f);
+		glVertex3f(0.0f,2.75f,0.0f);
+		glVertex3f(0.0f,-2.75f,0.0f);
+	glEnd();
+	glPointSize(6);
+	glBegin(GL_POINTS);
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f(0.0f,fx,0.0f);
+	glEnd();
+	glPopMatrix();
+	
+}
+void drawArrowAxes(){
 	int left=1,right=1,top=1,bottom = 1;
-    glPushMatrix();
-    float neworix = (0.0f - startx)*10/(stopx-startx)-5.0f; //transforms the position of y axis on screen depending on input range of function.
+	
+	
+	
+	float neworix = (0.0f - startx)*10/(stopx-startx)-5.0f; //transforms the position of y axis on screen depending on input range of function.
     if(neworix>=5.0f){
 	    neworix = 5.0f;	
 	    right = 0;
@@ -91,7 +112,9 @@ void drawScene() {
 		neworix = -5.0f;
 		left = 0;
 	}
-	float neworiy = (0.0f - starty)*5.5/(stopy-starty)-2.75f;
+	
+	float neworiy = (0.0f - starty)*5.5/(stopy-starty)-2.75f; //transforms the position of x axis on screen depending on y values.
+	
 	if(neworiy>=2.75f){
 	    neworiy = 2.75f;
 	    top = 0;	
@@ -99,85 +122,115 @@ void drawScene() {
 		neworiy = -2.75f;
 		bottom = 0;
 	}
-    glTranslatef(neworix, 0.0f, 0.0f);
+	
+	glPushMatrix();
+	glTranslatef(neworix, 0.0f, 0.0f);
+	
 	glBegin(GL_LINES);
 		glVertex3f(0.0f, -2.80f, 0.0f);
 		glVertex3f(0.0f, 2.80f, 0.0f);
 	glEnd();
-	if(bottom){
-	glBegin(GL_LINE_STRIP);
-        glVertex3f(-0.1f,-2.75f,0.0f);
+	
+	if(bottom){ //Render bottom arrow?
+	glBegin(GL_TRIANGLES);
+        glVertex3f(-0.04f,-2.70f,0.0f);
         glVertex3f(0.0f,-2.80f,0.0f);
-        glVertex3f(0.1f,-2.75f,0.0f);
+        glVertex3f(0.04f,-2.70f,0.0f);
 	glEnd();
 	}
-	if(top){
-	glBegin(GL_LINE_STRIP);
-        glVertex3f(-0.1f,2.75f,0.0f);
+	
+	if(top){ //Render top arrow?
+	glBegin(GL_TRIANGLES);
+        glVertex3f(-0.04f,2.70f,0.0f);
         glVertex3f(0.0f,2.80f,0.0f);
-        glVertex3f(0.1f,2.75f,0.0f);
+        glVertex3f(0.04f,2.70f,0.0f);
 	glEnd();
 	}
 	glPopMatrix();
+	
 	glPushMatrix();
 	glTranslatef(0.0f,neworiy,0.0f);
 	glBegin(GL_LINES);
 		glVertex3f(-5.1f, 0.0f, 0.0f);
 		glVertex3f(5.1f, 0.0f, 0.0f);
 	glEnd();
-	if(left){
-	glBegin(GL_LINE_STRIP);
-        glVertex3f(-5.0,0.1,0.0f);
+	
+	if(left){ //Render left arrow?
+	glBegin(GL_TRIANGLES);
+        glVertex3f(-5.0,0.04,0.0f);
         glVertex3f(-5.1f,0.0f,0.0f);
-        glVertex3f(-5.0f,-0.1f,0.0f);
+        glVertex3f(-5.0f,-0.04f,0.0f);
 	glEnd();
 	}
-	if(right){
-	glBegin(GL_LINE_STRIP);
-        glVertex3f(5.0,0.1,0.0f);
+	
+	if(right){ //Render right arrow?
+	glBegin(GL_TRIANGLES);
+        glVertex3f(5.0,0.04,0.0f);
         glVertex3f(5.1f,0.0f,0.0f);
-        glVertex3f(5.0f,-0.1f,0.0f);
+        glVertex3f(5.0f,-0.04f,0.0f);
 	glEnd();
 	}
 	glPopMatrix();
+}
+
+void drawScene() {
+	printf("Draw\n");
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	
+	glLoadIdentity();
+	
+	glTranslatef(0.0f, 0.0f, -7.0f);
+	glColor3f(1.0f,1.0f,1.0f);
+	
+	drawArrowAxes();
+	
+	
+	
 	glBegin(GL_LINE_STRIP);
         int i;
         float x=startx; //Actual value of x of function.
         for(i=0;i<segments;i++){
             float xdisp = screenxstart + 10*(x-startx)/(stopx-startx); //Corresponding value of x on screen.
-            float ydisp = screenystart + 5.5*(preplot[i]-starty)/(stopy-starty);
+            float ydisp = screenystart + 5.5*(y[i]-starty)/(stopy-starty);
             glVertex3f(xdisp,ydisp,0.0f);
             x += (stopx-startx)/segments;
         }
     glEnd();
+    
+    drawPointLoc();
 
 	glutSwapBuffers();
+	glutPostRedisplay();
 }
 
 void update(int value) {
-	/*
-	ang += 1;
-	if (ang > 360.0f) {
-		ang = 0.0f;
-	}
-	*/
 	glutPostRedisplay();
 	glutTimerFunc(166, update, 0);
 }
 
 int main(int argc, char** argv) {
+	
     inpfunc();
     precompute();
+    
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(200, 200);
+	
 	initRendering();
 	glutCreateWindow("Graph Plotter");
 	glutFullScreen();
+	
+	glutPassiveMotionFunc(mouseMotion);
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
 	glutReshapeFunc(handleResize);
+	
+	
 	glutMainLoop();
+	
 	return 0;
 }
